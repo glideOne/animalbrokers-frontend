42100a7a044117e551b25eb27f05a3d99b5ef8f2
Ovidiu Zakarias <ovidiu.zakarias@pitechnologies.ro>
2021-06-01 23:47:57 +0300

    Added dashboard.

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <br>
+    <h1>Animal Brokers - Dashboard</h1>
+  </div>
+
+<!--  <v-btn-->
+<!--      elevation="2"-->
+<!--  ></v-btn>-->
+
+</template>
+
+<script>
+export default {
+  name: "Dashboard"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,18 +9,18 @@
         <form>
           <div class="mb-3">
 <!--            <label for="exampleInputEmail1" class="form-label">Email address</label>-->
-            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="E-mail" aria-describedby="emailHelp" required>
+            <input type="email" v-model="email" class="form-control" id="exampleInputEmail1" placeholder="E-mail" aria-describedby="emailHelp" required>
 <!--            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>-->
           </div>
           <div class="mb-3">
 <!--            <label for="exampleInputPassword1" class="form-label">Password</label>-->
-            <input type="password" class="form-control" placeholder="Password" id="exampleInputPassword1" required>
+            <input type="password" v-model="password" class="form-control" placeholder="Password" id="exampleInputPassword1" required>
           </div>
 <!--          <div class="mb-3 form-check">-->
 <!--            <input type="checkbox" class="form-check-input" id="exampleCheck1">-->
 <!--            <label class="form-check-label" for="exampleCheck1">Check me out</label>-->
 <!--          </div>-->
-          <button type="submit" class="btn btn-primary">Login</button>
+          <button v-on:click.prevent="login" class="btn btn-primary">Login</button>
         </form>
       </div>
       <div class="col-md-3"></div>
@@ -40,7 +40,21 @@ export default {
 
   methods: {
     login: function () {
-      console.log("Email: " + this.email);
+      this.$http.post('/api/v1/auth/login', {
+        email: this.email,
+        password: this.password
+      }, {
+        headers: {
+
+        }
+      }).then(function (response) {
+        console.log(response);
+        this.submitted = true;
+
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('user', JSON.stringify(response.data.userDto))
+      }).then(() => this.$router.push(this.$route.query.redirect || '/dashboard'))
+        .catch(error => { console.log(error) })
     }
   }
 }
