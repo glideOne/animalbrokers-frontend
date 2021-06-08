6c7a9bf2fe38f1f7494d3243b23387bca9616bdf
Ovidiu Zakarias <ovidiu.zakarias@pitechnologies.ro>
2021-06-08 11:17:48 +0300

    Added maps.

--- src/main.js
+++ src/main.js
@@ -3,28 +3,37 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import firebase from "firebase";
+// import firebase from "firebase";
 import VueResource from "vue-resource";
-import vuetify from './plugins/vuetify'
+import vuetify from './plugins/vuetify';
+import * as VueGoogleMaps from 'vue2-google-maps';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyBwiITw2LKvW9AQkmv5bT-D6S5c-saWAdw',
+    libraries: 'places'
+  },
+  installComponents: true,
+})
 
 Vue.use(VueResource)
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-  apiKey: "AIzaSyAf-bGiHHW-mZUfUkIros8Am9bydTRU2cw",
-  authDomain: "animalbrokers-ab92b.firebaseapp.com",
-  projectId: "animalbrokers-ab92b",
-  storageBucket: "animalbrokers-ab92b.appspot.com",
-  messagingSenderId: "149958519538",
-  appId: "1:149958519538:web:b336055bb69359aa6d7e6b",
-  measurementId: "G-GQKLM85DDF"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-console.log(firebase);
+// var firebaseConfig = {
+//   apiKey: "AIzaSyAf-bGiHHW-mZUfUkIros8Am9bydTRU2cw",
+//   authDomain: "animalbrokers-ab92b.firebaseapp.com",
+//   projectId: "animalbrokers-ab92b",
+//   storageBucket: "animalbrokers-ab92b.appspot.com",
+//   messagingSenderId: "149958519538",
+//   appId: "1:149958519538:web:b336055bb69359aa6d7e6b",
+//   measurementId: "G-GQKLM85DDF"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// console.log(firebase);
 
 new Vue({
   router,
--- src/views/CreateThread.vue
+++ src/views/CreateThread.vue
@@ -3,17 +3,20 @@
 
     <template>
       <form>
-
         <template>
           <v-form>
             <v-container>
+              <v-col>
+                <h2>Create New Thread</h2>
+              </v-col>
+
               <v-row>
                 <v-col
                     cols="12"
                     sm="6"
                 >
                   <v-text-field
-                      v-model="text"
+                      v-model="title"
                       :counter="160"
                       label="Thread title"
                       required
@@ -67,26 +70,63 @@
                     sm="6"
                 >
                   <v-text-field
-                      v-model="last"
-                      label="Last Name"
+                      v-model="petName"
+                      label="Pet name"
                       solo-inverted
                   ></v-text-field>
 
+                  <v-col
+                      align="left"
+                  ><h5>Date and time of last sighting</h5>
+                  </v-col>
                   <v-row justify="center">
-                    <v-date-picker v-model="picker"></v-date-picker>
+                    <v-date-picker v-model="lastSeenDate"></v-date-picker>
                   </v-row>
 
                 </v-col>
               </v-row>
 
               <v-row>
-                <div style="height: 400pt; border: 1pt solid black"></div>
+                <v-col
+                    align="left"
+                ><h5>Last known location:</h5>
+                </v-col>
+                <div>
+                  <GmapMap
+                      ref="mapRef"
+                      :center="{lat:46.7712, lng:23.6236}"
+                      :zoom="12"
+                      :options="mapOptions"
+                      map-type-id="terrain"
+                      style="height: 350pt"
+                      @click="handleMapClick"
+                  >n
+                    <GmapMarker
+                        :position="marker.position"
+                        :clickable="true"
+                        :draggable="true"
+                        @drag="handleMarkerDrag"
+                        @click="panToMarker"
+                    />
+                  </GmapMap>
+                </div>
               </v-row>
+
+              <v-col></v-col>
+
+              <v-col style="">
+                <v-btn
+                    color="primary"
+                    elevation="2"
+                    large
+                    x-large
+                >Create thread
+                </v-btn>
+              </v-col>
 
             </v-container>
           </v-form>
         </template>
-
       </form>
     </template>
 
@@ -101,7 +141,15 @@ export default {
     return {
       threadTypes: [],
       animalClasses: [],
-      animalBreeds: []
+      animalBreeds: [],
+      title: "",
+      petName: "",
+      lastSeenDate: "",
+      lastKnownLocation: {latitude: 0, longitude: 0},
+      marker: {position: {lat: 10, lng: 10}},
+      mapOptions: {
+        disableDefaultUI: true,
+      }
     }
   },
 
@@ -137,7 +185,24 @@ export default {
           }).then(function (response) {
         this.animalBreeds = response.data
       })
-    }
+    },
+
+    //sets the position of marker when dragged
+    handleMarkerDrag(e) {
+      this.marker.position = {lat: e.latLng.lat(), lng: e.latLng.lng()};
+    },
+
+    //Moves the map view port to marker
+    panToMarker() {
+      this.$refs.mapRef.panTo(this.marker.position);
+    },
+
+    //Moves the marker to click position on the map
+    handleMapClick(e) {
+      this.marker.position = {lat: e.latLng.lat(), lng: e.latLng.lng()};
+      console.log(e.latLng.lat());
+      console.log(e.latLng.lng());
+    },
 
   },
 
