0e67cd2bce822cb7cdbc46c56cf9ba2eccf94f6d
Ovidiu Zakarias <ovidiu.zakarias@gmail.com>
2021-06-09 16:54:46 +0300

    Removed Firebase stuff.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-// import firebase from "firebase";
 import VueResource from "vue-resource";
 import vuetify from './plugins/vuetify';
 import * as VueGoogleMaps from 'vue2-google-maps';
@@ -12,28 +11,13 @@ Vue.config.productionTip = false;
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyAP6ZWfkpeydLqVfgq8LXF-6RKMobxLP84',
+    key: '',
     libraries: 'places'
   },
   installComponents: true,
 })
 
 Vue.use(VueResource)
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// var firebaseConfig = {
-//   apiKey: "AIzaSyAf-bGiHHW-mZUfUkIros8Am9bydTRU2cw",
-//   authDomain: "animalbrokers-ab92b.firebaseapp.com",
-//   projectId: "animalbrokers-ab92b",
-//   storageBucket: "animalbrokers-ab92b.appspot.com",
-//   messagingSenderId: "149958519538",
-//   appId: "1:149958519538:web:b336055bb69359aa6d7e6b",
-//   measurementId: "G-GQKLM85DDF"
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-// console.log(firebase);
 
 new Vue({
   router,
